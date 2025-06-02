6148b0cd1175b4931e37ab12091ec53deac3541f
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-06-03 00:58:47 +0530

    Time: 5 ms (14.23%), Space: 45.8 MB (57.81%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,29 +1,25 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int[] candies = new int[n];
-        Arrays.fill(candies, 1);
+        int[] lc = new int[n];
+        int[] rc = new int[n];
+        Arrays.fill(lc, 1);
+        Arrays.fill(rc, 1);
 
-        boolean changed = true;
-        while(changed){
-            changed = false;
-
-            for(int i=0; i<n; i++){
-                if(i>0 && ratings[i] > ratings[i-1] && candies[i] <= candies[i-1]){
-                    candies[i] = candies[i-1]+1;
-                    changed = true;
-                }
-
-                if(i<n-1 && ratings[i] > ratings[i+1] && candies[i] <= candies[i+1]){
-                    candies[i] = candies[i+1]+1;
-                    changed = true;
-                }
+        for(int i=1; i<n; i++){
+            if(ratings[i] > ratings[i-1]){
+                lc[i] = lc[i-1]+1;
+            }
+        }
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                rc[i] = rc[i+1]+1;
             }
         }
 
         int totalCandies = 0;
-        for (int candy : candies) {
-            totalCandies += candy;
+        for (int i=0; i<n; i++) {
+            totalCandies += Math.max(lc[i], rc[i]);
         }
 
         return totalCandies;

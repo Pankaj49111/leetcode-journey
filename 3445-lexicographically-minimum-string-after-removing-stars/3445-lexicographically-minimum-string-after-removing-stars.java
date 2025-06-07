1e660a246682143e5daae0be029b39b86f9916bc
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-06-07 14:25:00 +0530

    Time: 146 ms (79.39%), Space: 54.7 MB (23.03%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.java
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.java
@@ -1,30 +1,32 @@
 class Solution {
-
     public String clearStars(String s) {
-        Deque<Integer>[] cnt = new Deque[26];
-        for (int i = 0; i < 26; i++) {
-            cnt[i] = new ArrayDeque<>();
-        }
-        char[] arr = s.toCharArray();
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != '*') {
-                cnt[arr[i] - 'a'].push(i);
+        int n = s.length();
+        PriorityQueue<Character> pq = new PriorityQueue<>();
+        Map<Character, Deque<Integer>> map = new HashMap<>();
+        boolean[] keep = new boolean[n];
+        Arrays.fill(keep, true);
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '*') {
+                char smallest = pq.poll();
+                int indexToRemove = map.get(smallest).removeLast();
+                keep[i] = false;               // Remove '*'
+                keep[indexToRemove] = false;   // Remove smallest char
             } else {
-                for (int j = 0; j < 26; j++) {
-                    if (!cnt[j].isEmpty()) {
-                        arr[cnt[j].pop()] = '*';
-                        break;
-                    }
-                }
+                pq.offer(c);
+                map.putIfAbsent(c, new ArrayDeque<>());
+                map.get(c).add(i);
             }
         }
 
-        StringBuilder ans = new StringBuilder();
-        for (char c : arr) {
-            if (c != '*') {
-                ans.append(c);
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (keep[i]) {
+                result.append(s.charAt(i));
             }
         }
-        return ans.toString();
+
+        return result.toString();
     }
 }

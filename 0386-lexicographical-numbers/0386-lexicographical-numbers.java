3849f53333f30714f967a234ab2646a8a9e75212
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-06-08 18:50:26 +0530

    Time: 1 ms (100%), Space: 48.1 MB (88.85%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,48 +1,18 @@
 class Solution {
-    class TrieNode {
-        TrieNode[] children = new TrieNode[10];
-        boolean isEnd = false;
-    }
-
-    private void insert(TrieNode root, int num) {
-        String s = Integer.toString(num);
-        TrieNode node = root;
-        for (char c : s.toCharArray()) {
-            int idx = c - '0';
-            if (node.children[idx] == null) {
-                node.children[idx] = new TrieNode();
-            }
-            node = node.children[idx];
-        }
-        node.isEnd = true;
-    }
-
-    // DFS traversal of the Trie
-    private void dfs(TrieNode node, String path, List<Integer> result) {
-        if (node.isEnd) {
-            result.add(Integer.parseInt(path));
-        }
-
-        for (int i = 0; i <= 9; i++) {
-            if (node.children[i] != null) {
-                dfs(node.children[i], path + i, result);
-            }
-        }
-    }
-
     public List<Integer> lexicalOrder(int n) {
-        TrieNode root = new TrieNode();
-        for (int i = 1; i <= n; i++) {
-            insert(root, i);
-        }
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 1; i < 10; i++) 
+            solve(i, n, ans);
+        return ans;
+    }
 
-        List<Integer> result = new ArrayList<>();
-        for (int i = 1; i <= 9; i++) {
-            if (root.children[i] != null) {
-                dfs(root.children[i], "" + i, result);
-            }
+    void solve(int prod, int n, List<Integer> ans) {
+        if (prod > n) return;
+        ans.add(prod);
+        for (int j = 0; j < 10; j++) {
+            int next = prod * 10 + j;
+            if(next > n) return;
+            solve(next, n, ans);
         }
-
-        return result;
     }
 }

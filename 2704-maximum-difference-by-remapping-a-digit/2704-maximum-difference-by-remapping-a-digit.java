7b8d6cf59fd5faebe95aa1487d2a2b477ee5f5a9
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-06-14 16:31:45 +0530

    Time: 1 ms (89.2%), Space: 40.3 MB (95.45%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.java
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.java
@@ -1,24 +1,37 @@
 class Solution {
     public int minMaxDifference(int num) {
-        String s = String.valueOf(num);
+        String str = Integer.toString(num);
+        char[] maxChars = str.toCharArray();
+        char[] minChars = str.toCharArray();
 
-        String maxS = "", minS = "";
-
-        for(char c: s.toCharArray()){
-            if(c != '9'){
-                maxS = s.replaceAll(""+c, "9");
+        // Step 1: Find first non-9 digit for max replacement
+        char replaceForMax = ' ';
+        for (char c : maxChars) {
+            if (c != '9') {
+                replaceForMax = c;
                 break;
             }
         }
-        if(maxS == "") maxS = ""+s;
-        for(char c: s.toCharArray()){
-            if(c != '0'){
-                minS = s.replaceAll(""+c, "0");
-                break;
+
+        // Step 2: Replace that digit with 9 for max
+        for (int i = 0; i < maxChars.length; i++) {
+            if (maxChars[i] == replaceForMax) {
+                maxChars[i] = '9';
             }
         }
-        if(minS == "") minS = ""+s;
 
-        return Integer.parseInt(maxS) - Integer.parseInt(minS);
+        // Step 3: Replace first digit for min with 0
+        char replaceForMin = minChars[0];
+        for (int i = 0; i < minChars.length; i++) {
+            if (minChars[i] == replaceForMin) {
+                minChars[i] = '0';
+            }
+        }
+
+        // Step 4: Convert and calculate result
+        int maxVal = Integer.parseInt(new String(maxChars));
+        int minVal = Integer.parseInt(new String(minChars));
+
+        return maxVal - minVal;
     }
 }

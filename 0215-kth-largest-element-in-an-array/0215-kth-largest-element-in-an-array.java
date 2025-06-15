86bcdb93e8584418bc73cf0ff9d46395f47d953c
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-06-16 00:40:57 +0530

    Time: 11 ms (94.86%), Space: 57.6 MB (74.89%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,46 +1,40 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int n = nums.length;
-        return quickSelect(nums, 0, n - 1, n - k); // kth largest == (n-k)th smallest
+        int targetIdx = nums.length - k;
+        return quickSelect(nums, 0, nums.length - 1, targetIdx);
     }
-
-    private int quickSelect(int[] nums, int left, int right, int kSmallest) {
-        if (left == right) return nums[left];
-
-        // Random pivot
-        int pivotIndex = left + new Random().nextInt(right - left + 1);
-        pivotIndex = partition(nums, left, right, pivotIndex);
-
-        if (pivotIndex == kSmallest) {
-            return nums[pivotIndex];
-        } else if (pivotIndex > kSmallest) {
-            return quickSelect(nums, left, pivotIndex - 1, kSmallest);
-        } else {
-            return quickSelect(nums, pivotIndex + 1, right, kSmallest);
+        
+    private int quickSelect(int[] nums, int left, int right, int targetIdx) {
+        if (left == right) {
+            return nums[left];
         }
-    }
 
-    private int partition(int[] nums, int left, int right, int pivotIndex) {
-        int pivot = nums[pivotIndex];
-        // Move pivot to the end
-        swap(nums, pivotIndex, right);
-        int storeIndex = left;
+        int pivot = nums[left];
+        int low = left;
+        int high = right;
 
-        for (int i = left; i < right; i++) {
-            if (nums[i] < pivot) {
-                swap(nums, storeIndex, i);
-                storeIndex++;
+        while (low <= high) {
+            while (low <= high && nums[low] < pivot) {
+                low++;
+            }
+            while (low <= high && nums[high] > pivot) {
+                high--;
+            }
+            if (low <= high) {
+                int temp = nums[low];
+                nums[low] = nums[high];
+                nums[high] = temp;
+                low++;
+                high--;
             }
         }
 
-        // Move pivot back to its place
-        swap(nums, storeIndex, right);
-        return storeIndex;
-    }
-
-    private void swap(int[] nums, int a, int b) {
-        int tmp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = tmp;
+        if (targetIdx <= high) {
+            return quickSelect(nums, left, high, targetIdx);
+        } else if (targetIdx >= low) {
+            return quickSelect(nums, low, right, targetIdx);
+        } else {
+            return nums[targetIdx];
+        }
     }
 }

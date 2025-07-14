f9398fd48d5f644e50f7e341301ffbfdd7cde9e6
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-07-15 03:18:34 +0530

    Time: 84 ms (56.94%), Space: 78.7 MB (48.33%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -2,24 +2,45 @@ class Solution {
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
         List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
-        for(int[] edge: edges){
-            graph.get(edge[1]).add(edge[0]);
+
+        int[] indegree = new int[n];
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            indegree[edge[1]]++;
         }
 
-        List<List<Integer>> res = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            Set<Integer> ancs = new HashSet<>();
-            dfs(graph, i, ancs);
-            List<Integer> ls = new ArrayList<>(ancs);
-            Collections.sort(ls);
-            res.add(ls);
-        }
-        return res;
-    }
+        // Each node will have a set of ancestors
+        List<Set<Integer>> ancestors = new ArrayList<>();
+        for (int i = 0; i < n; i++) ancestors.add(new HashSet<>());
 
-    void dfs(List<List<Integer>> graph, int idx, Set<Integer> ancs){
-        for(int prev: graph.get(idx)){
-            if(ancs.add(prev)) dfs(graph, prev, ancs);
+        // Topological sort
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) queue.add(i);
         }
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            for (int neighbor : graph.get(node)) {
+                // Add all ancestors of node into neighbor
+                ancestors.get(neighbor).add(node);
+                ancestors.get(neighbor).addAll(ancestors.get(node));
+
+                // Decrease indegree and enqueue if 0
+                if (--indegree[neighbor] == 0) {
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        // Convert sets to sorted lists
+        List<List<Integer>> result = new ArrayList<>();
+        for (Set<Integer> set : ancestors) {
+            List<Integer> list = new ArrayList<>(set);
+            Collections.sort(list);
+            result.add(list);
+        }
+
+        return result;
     }
 }

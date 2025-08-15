7152f168449722d93723b79a52104f81f8146a48
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-08-16 02:41:02 +0530

    Time: 49 ms (68.64%), Space: 55.3 MB (40.68%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -5,22 +5,14 @@ class Solution {
         int max = -1;
 
         while(j<n){
-            // if(map.size() <= 2) {
-                if(!map.containsKey(fruits[j])){
-                    map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
-                    while(map.size() > 2){
-                        map.put(fruits[i], map.getOrDefault(fruits[i], 0) - 1);
-                        if(map.get(fruits[i]) == 0) map.remove(fruits[i]);
-                        i++;
-                    }
-                    j++;
-                    max = Math.max(max, j-i);
-                } else {
-                    map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
-                    j++;
-                    max = Math.max(max, j-i);
-                }
-            // }
+            map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
+            while(map.size() > 2){
+                map.put(fruits[i], map.getOrDefault(fruits[i], 0) - 1);
+                if(map.get(fruits[i]) == 0) map.remove(fruits[i]);
+                i++;
+            }
+            j++;
+            max = Math.max(max, j-i);
         }
         return max;
     }

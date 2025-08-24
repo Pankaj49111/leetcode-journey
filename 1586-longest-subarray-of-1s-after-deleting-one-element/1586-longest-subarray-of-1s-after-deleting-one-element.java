fffe1200382eb00ff13a40fb44e09fe8305968d1
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-08-25 02:18:06 +0530

    Time: 24 ms (5.23%), Space: 57.8 MB (6.82%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,21 +1,29 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int dp1 = 0;  // streak of 1s with no deletion yet
-        int dp0 = 0;  // streak of 1s after one deletion
+        int n = nums.length;
+        int[][] dp = new int[n][2]; // dp[i][0] = no deletion, dp[i][1] = one deletion
         int max = 0;
 
-        for (int num : nums) {
-            if (num == 1) {
-                dp1++;
-                dp0++;
-            } else {
-                dp0 = dp1;  // simulate deletion of this 0
-                dp1 = 0;
-            }
-            max = Math.max(max, dp0);
+        if (nums[0] == 1) {
+            dp[0][0] = 1;
         }
 
-        // Handle case when all are 1s → must delete one
-        return max == nums.length ? max - 1 : max;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == 1) {
+                dp[i][0] = dp[i - 1][0] + 1;
+                dp[i][1] = dp[i - 1][1] + 1;
+            } else {
+                dp[i][0] = 0; // no-deletion can't continue
+                dp[i][1] = dp[i - 1][0]; // delete this 0
+            }
+            max = Math.max(max, dp[i][1]);
+        }
+
+        if (max == 0 && Arrays.stream(nums).anyMatch(num -> num == 1)) {
+            return n - 1;
+        }
+
+        return max;
     }
+
 }

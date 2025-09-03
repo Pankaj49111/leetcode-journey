41e543471ae262f4a7d63aaa7e3d20e167586f22
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-09-04 04:10:27 +0530

    Time: 10 ms (84.19%), Space: 45.6 MB (67.7%) - LeetHub

--- 2317-count-collisions-on-a-road/2317-count-collisions-on-a-road.java
+++ 2317-count-collisions-on-a-road/2317-count-collisions-on-a-road.java
@@ -1,41 +1,22 @@
 class Solution {
     public int countCollisions(String directions) {
-        Deque<Character> st = new ArrayDeque<>();
-        int collisions = 0;
+        int n = directions.length();
+        int left = 0, right = n - 1;
 
-        for (char c : directions.toCharArray()) {
-            if (c == 'R') {
-                st.push('R');
-            } else if (c == 'S') {
-                // All pending R's collide into this S
-                while (!st.isEmpty() && st.peek() == 'R') {
-                    st.pop();
-                    collisions += 1;
-                }
-                st.push('S');
-            } else { // c == 'L'
-                if (st.isEmpty()) {
-                    // Leaves the road, no collision
-                    continue;
-                }
-                if (st.peek() == 'R') {
-                    // First R + L -> 2 collisions
-                    st.pop();
-                    collisions += 2;
-                    // Any remaining R's behind will bump into the new S
-                    while (!st.isEmpty() && st.peek() == 'R') {
-                        st.pop();
-                        collisions += 1;
-                    }
-                    st.push('S');
-                } else if (st.peek() == 'S') {
-                    // L meets S -> 1 collision, remains S
-                    collisions += 1;
-                    // Keep a single S on top; no need to push another
-                } else {
-                    // top is 'L' -> current L also leaves, no collision
-                    // do nothing
-                }
+        // Skip escaping left movers
+        while (left < n && directions.charAt(left) == 'L') {
+            left++;
+        }
+
+        // Skip escaping right movers
+        while (right >= 0 && directions.charAt(right) == 'R') {
+            right--;
+        }
+
+        int collisions = 0;
+        for (int i = left; i <= right; i++) {
+            if (directions.charAt(i) != 'S') {
+                collisions++;
             }
         }
         return collisions;

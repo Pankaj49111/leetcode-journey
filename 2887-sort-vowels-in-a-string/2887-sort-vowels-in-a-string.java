0348416a66a2ea246f7e9573fd2d1f1f2cc174f9
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-09-12 03:02:50 +0530

    Time: 191 ms (18.27%), Space: 52.6 MB (5.56%) - LeetHub

---
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String sortVowels(String s) {
+        Queue<Character> cpq = new PriorityQueue<>();
+        Queue<Integer> ipq = new PriorityQueue<>();
+
+        String vowels = "aeiouAEIOU";
+        for(int i=0; i<s.length(); i++){
+            if(vowels.indexOf(s.charAt(i)) != -1){
+                cpq.offer(s.charAt(i));
+                ipq.add(i);
+            }
+        }
+        List<Character> ls = new ArrayList<>();
+
+        for(int i=0; i<s.length(); i++){
+            if(!ipq.isEmpty() && i == ipq.peek()){
+                ls.add(ipq.poll(), cpq.poll());
+            } else {
+                ls.add(s.charAt(i));
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Character c : ls) {
+            sb.append(c);
+        }
+
+        String result = sb.toString();
+        // System.out.println(result);
+        // while(!cpq.isEmpty()){
+        //     System.out.println(cpq.poll()+" : "+ipq.poll());
+        // }
+        return result;
+    }
+}

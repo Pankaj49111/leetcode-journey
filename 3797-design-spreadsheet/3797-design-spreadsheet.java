394d5163932502ced67b39c9a94cdc43152b18b6
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-09-19 17:25:53 +0530

    Time: 131 ms (29.93%), Space: 55.6 MB (98.24%) - LeetHub

--- 3797-design-spreadsheet/3797-design-spreadsheet.java
+++ 3797-design-spreadsheet/3797-design-spreadsheet.java
@@ -1,36 +1,42 @@
 class Spreadsheet {
     int[][] arr;
+
     public Spreadsheet(int rows) {
         arr = new int[rows][26];
     }
-    
+
     public void setCell(String cell, int value) {
-        int col = cell.charAt(0) - 'A';
-        int row = Integer.parseInt(cell.substring(1)) - 1;
-        arr[row][col] = value;
+        int[] pos = parseCell(cell);
+        arr[pos[0]][pos[1]] = value;
     }
-    
+
     public void resetCell(String cell) {
-        int col = cell.charAt(0) - 'A';
-        int row = Integer.parseInt(cell.substring(1)) - 1;
-        arr[row][col] = 0;
+        int[] pos = parseCell(cell);
+        arr[pos[0]][pos[1]] = 0;
     }
-    
+
     public int getValue(String formula) {
         formula = formula.replace("=", "");
-        String[] form = formula.split("\\+");
-        int res=0;
-        for(String cell: form){
-            cell = cell.toUpperCase();
-            if(Character.isLetter(cell.charAt(0))){
-                int col = cell.charAt(0) - 'A';
-                int row = Integer.parseInt(cell.substring(1)) - 1;
-                res += arr[row][col];
+        String[] parts = formula.split("\\+");
+        int res = 0;
+
+        for (String part : parts) {
+            part = part.toUpperCase();
+            if (Character.isLetter(part.charAt(0))) {
+                int[] pos = parseCell(part);
+                res += arr[pos[0]][pos[1]];
             } else {
-                res += Integer.parseInt(cell);
+                res += Integer.parseInt(part);
             }
         }
+
         return res;
+    }
+
+    int[] parseCell(String cell) {
+        int col = cell.charAt(0) - 'A';
+        int row = Integer.parseInt(cell.substring(1)) - 1;
+        return new int[]{row, col};
     }
 }
 

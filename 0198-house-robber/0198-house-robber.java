21e825b804b827dfe53f5a7d01a6aa731c91e6f6
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-10-19 03:12:32 +0530

    Time: 0 ms (100%), Space: 41.4 MB (25.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,19 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
-        Integer[] memo = new Integer[nums.length];
-        return dfs(nums, 0, memo);
-    }
+        int n = nums.length;
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
 
-    private int dfs(int[] nums, int i, Integer[] memo) {
-        if (i >= nums.length) return 0;
-        if (memo[i] != null) return memo[i];
+        int prev2 = 0;       // dp[i-2]
+        int prev1 = 0;       // dp[i-1]
+        int curr = 0;
 
-        // option1: rob current, skip next
-        int take = nums[i] + dfs(nums, i + 2, memo);
-        // option2: skip current
-        int skip = dfs(nums, i + 1, memo);
-
-        memo[i] = Math.max(take, skip);
-        return memo[i];
+        for (int x : nums) {
+            curr = Math.max(prev1, prev2 + x);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return curr;
     }
 }

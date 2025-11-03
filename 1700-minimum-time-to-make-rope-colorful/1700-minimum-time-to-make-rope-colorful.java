94d4571bb5d0883cf83e20d5c883840ea131c721
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-11-04 01:14:18 +0530

    Time: 8 ms (82.47%), Space: 90.7 MB (5.17%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.java
@@ -1,16 +1,18 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
-        int count=0;
-        if(colors.length() == 1) return count;
-
-        char[] ch = colors.toCharArray();
-        for(int i=1; i<ch.length; i++){
-            if(ch[i] == ch[i-1]){
-                count += Math.min(neededTime[i], neededTime[i-1]);
-
-                neededTime[i] = Math.max(neededTime[i], neededTime[i-1]);
+        int totalTime = 0;
+        int n = colors.length();
+        
+        for (int i = 1; i < n; i++) {
+            if (colors.charAt(i) == colors.charAt(i - 1)) {
+                // Remove the smaller one
+                totalTime += Math.min(neededTime[i], neededTime[i - 1]);
+                
+                // Keep the higher time for future comparison
+                neededTime[i] = Math.max(neededTime[i], neededTime[i - 1]);
             }
         }
-        return count;
+        
+        return totalTime;
     }
 }

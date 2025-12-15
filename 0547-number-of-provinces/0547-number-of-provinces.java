12e142b1c0c9cc99d7d0fbfc4beacbcf0dc6ec01
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-12-16 01:24:11 +0530

    Time: 1 ms (86.1%), Space: 47.4 MB (49.33%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,24 +1,34 @@
 class Solution {
+    int[] parent;
     public int findCircleNum(int[][] isConn) {
         int n=isConn.length;
-        boolean[] vis=new boolean[n];
-        int prov=0;
+        parent = new int[n];
+
+        for(int i=0; i<n; i++) parent[i] = i;
 
         for(int i=0; i<n; i++){
-            if(!vis[i]){
-                dfs(isConn, vis, i);
-                prov++;
+            for(int j=i+1; j<n; j++){
+                if(isConn[i][j] == 1) union(i,j);
             }
+        }
+
+        int prov=0;
+        for(int i=0; i<n; i++){
+            if(parent[i] == i) prov++;
         }
         return prov;
     }
 
-    void dfs(int[][] graph, boolean[] vis, int idx){
-        vis[idx] = true;
-        for(int j=0; j<graph.length; j++){
-            if(graph[idx][j] == 1 && !vis[j]){
-                dfs(graph, vis, j);
-            }
+    int find(int x){
+        if(parent[x] != x){
+            parent[x] = find(parent[x]);
         }
+        return parent[x];
+    }
+
+    void union(int x, int y){
+        int px = find(x);
+        int py = find(y);
+        if(px != py) parent[py] = px;
     }
 }

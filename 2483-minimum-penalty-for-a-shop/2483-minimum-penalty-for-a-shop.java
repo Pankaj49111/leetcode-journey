72cf181d1b152ce0001107ad55ee44ed8889ee7f
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-12-28 22:21:33 +0530

    Time: 11 ms (56.21%), Space: 47 MB (38.8%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int bestClosingTime(String customers) {
+        int penalty = 0;
+        for (char c : customers.toCharArray()) {
+            if (c == 'Y') penalty++;
+        }
+
+        int minPenalty = penalty;
+        int bestHour = 0;
+
+        for (int i = 0; i < customers.length(); i++) {
+            if (customers.charAt(i) == 'Y') {
+                penalty--; // now open during a customer hour
+            } else {
+                penalty++; // open but no customers
+            }
+
+            if (penalty < minPenalty) {
+                minPenalty = penalty;
+                bestHour = i + 1; // closing AFTER this hour
+            }
+        }
+
+        return bestHour;
+    }
+}

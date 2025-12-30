b0f17b3446bdcfa15b83876366961d9284467e46
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2025-12-30 15:37:19 +0530

    Time: 1351 ms (38.51%), Space: 47 MB (65.54%) - LeetHub

---
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean pyramidTransition(String bottom, List<String> allowed) {
+        Map<String, List<Character>> map = new HashMap<>();
+
+        for (String rule : allowed) {
+            String key = rule.substring(0, 2);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(rule.charAt(2));
+        }
+        return dfs(bottom, map);
+    }
+
+    boolean dfs(String bottom, Map<String, List<Character>> map) {
+        if (bottom.length() == 1) {
+            return true;
+        }
+
+        return buildNext(bottom, 0, new StringBuilder(), map);
+    }
+
+    boolean buildNext(String bottom, int index, StringBuilder current,
+                    Map<String, List<Character>> map) {
+        if (index == bottom.length() - 1) {
+            return dfs(current.toString(), map);
+        }
+
+        String key = bottom.substring(index, index + 2);
+        if (!map.containsKey(key)) {
+            return false;
+        }
+
+        for (char c : map.get(key)) {
+            current.append(c);
+            if (buildNext(bottom, index + 1, current, map)) {
+                return true;
+            }
+            current.deleteCharAt(current.length() - 1);
+        }
+
+        return false;
+    }
+}

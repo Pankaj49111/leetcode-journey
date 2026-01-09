91f6902092ba388e0b11e1c1459a6caa2eff47b2
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2026-01-09 23:26:07 +0530

    Time: 77 ms (78.1%), Space: 91.5 MB (92.7%) - LeetHub

---
+++ 1686-stone-game-vi/1686-stone-game-vi.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int stoneGameVI(int[] av, int[] bv) {
+        int n = av.length;
+        Integer[] idx = new Integer[n];
+
+        for(int i=0; i<n; i++) idx[i] = i;
+
+        Arrays.sort(idx, (i,j) -> (av[j]+bv[j]) - (av[i]+bv[i]));
+
+        int aScore=0, bScore=0;
+
+        for(int turn=0; turn<n; turn++){
+            int stone = idx[turn];
+            if(turn%2 == 0){
+                aScore += av[stone];
+            } else {
+                bScore += bv[stone];
+            }
+        }
+        return Integer.compare(aScore, bScore);
+    }
+}

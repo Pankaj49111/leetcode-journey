50cb362dfa6e06b9aa9649dd3db7b375c5666a90
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2026-03-29 00:56:23 +0530

    Time: 9 ms (45.71%), Space: 173.1 MB (91.43%) - LeetHub

--- 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.java
+++ 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.java
@@ -1,10 +1,8 @@
 class Solution {
-    int[] parent;
-
     public String findTheString(int[][] lcp) {
         int n = lcp.length;
 
-        // Step 1: basic validation
+        // Step 1: validate diagonal + symmetry
         for (int i = 0; i < n; i++) {
             if (lcp[i][i] != n - i) return "";
             for (int j = 0; j < n; j++) {
@@ -12,63 +10,40 @@ class Solution {
             }
         }
 
-        // Step 2: DSU init
-        parent = new int[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
-
-        // Step 3: union
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (lcp[i][j] > 0) {
-                    union(i, j);
-                }
-            }
-        }
-
-        // Step 4: assign chars
+        // Step 2: build string greedily
         char[] word = new char[n];
+        Arrays.fill(word, '#');
+
         char ch = 'a';
 
-        int[] groupChar = new int[n];
-        Arrays.fill(groupChar, -1);
-
         for (int i = 0; i < n; i++) {
-            int p = find(i);
-            if (groupChar[p] == -1) {
+            if (word[i] == '#') {
                 if (ch > 'z') return "";
-                groupChar[p] = ch++;
+
+                for (int j = i; j < n; j++) {
+                    if (lcp[i][j] > 0) {
+                        word[j] = ch;
+                    }
+                }
+                ch++;
             }
-            word[i] = (char) groupChar[p];
         }
 
-        // Step 5: verify by recomputing LCP
-        int[][] calc = new int[n][n];
-
+        // Step 3: validate using LCP logic
         for (int i = n - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
                 if (word[i] == word[j]) {
-                    calc[i][j] = 1;
+                    int expected = 1;
                     if (i + 1 < n && j + 1 < n) {
-                        calc[i][j] += calc[i + 1][j + 1];
+                        expected += lcp[i + 1][j + 1];
                     }
+                    if (lcp[i][j] != expected) return "";
+                } else {
+                    if (lcp[i][j] != 0) return "";
                 }
             }
         }
 
-        // compare
-        for (int i = 0; i < n; i++) {
-            if (!Arrays.equals(calc[i], lcp[i])) return "";
-        }
-
         return new String(word);
-    }
-
-    int find(int x) {
-        if (parent[x] != x) parent[x] = find(parent[x]);
-        return parent[x];
-    }
-
-    void union(int a, int b) {
-        parent[find(a)] = find(b);
     }
 }

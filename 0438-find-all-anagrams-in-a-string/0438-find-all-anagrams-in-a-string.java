e077ca2408d6afd52c8b6a641cf50aa88104e513
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2026-09-03 01:50:34 +0530

    Time: 82 ms (14.94%), Space: 45.7 MB (99.87%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,36 +1,28 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> res = new ArrayList<>();
-        int n = p.length();
-        int m = s.length();
-        if (n > m) {
-            return res;
+        Map<Character, Integer> smap = new HashMap<>();
+        Map<Character, Integer> pmap = new HashMap<>();
+
+        if(s.length() < p.length()) return res;
+
+        for(int i=0; i<p.length(); i++){
+            smap.put(s.charAt(i), smap.getOrDefault(s.charAt(i), 0)+1);
+            pmap.put(p.charAt(i), pmap.getOrDefault(p.charAt(i), 0)+1);
         }
+        if(smap.equals(pmap)) res.add(0);
 
-        int[] pFreq = new int[26];
-        int[] windowFreq = new int[26];
+        int j=0, k=p.length();
+        while(k<s.length()){
+            char c1 = s.charAt(k++);
+            smap.put(c1, smap.getOrDefault(c1, 0)+1);
 
-        for (char c : p.toCharArray()) {
-            pFreq[c - 'a']++;
+            char c2 = s.charAt(j++);
+            smap.put(c2, smap.get(c2)-1);
+            if(smap.get(c2) == 0) smap.remove(c2);
+
+            if(smap.equals(pmap)) res.add(j);
         }
-
-        for (int i = 0; i < n; i++) {
-            windowFreq[s.charAt(i) - 'a']++;
-        }
-
-        if (Arrays.equals(pFreq, windowFreq)) {
-            res.add(0);
-        }
-
-        for (int i = n; i < m; i++) {
-            windowFreq[s.charAt(i) - 'a']++;
-            windowFreq[s.charAt(i - n) - 'a']--;
-
-            if (Arrays.equals(pFreq, windowFreq)) {
-                res.add(i - n + 1);
-            }
-        }
-
         return res;
     }
 }
